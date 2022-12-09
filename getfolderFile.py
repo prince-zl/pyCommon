fb33a8e07b3c3285ecfc31b4e9b55c477b82fe06
prince <2894708264@qq.com>
2022-12-09 17:47:36 +0800

    修改

--- getfolderFile.py
+++ getfolderFile.py
@@ -9,10 +9,12 @@ lis = os.listdir(file_folder_name)
 # a 追加 w写入
 with open(r'统计.txt', 'w', encoding='utf-8') as file_obj:
     for file_name in lis:
-        _file = os.listdir(
-            f'C:\\Users\\admin\\Desktop\\批量文章下载\\{file_name}')
-        _len = len(_file)
-        print(file_name+'：'+str(_len)+'篇')
-        file_obj.write(file_name+'：'+str(_len)+'篇'+'\n')
+        # 存在这个目录并且是文件夹则打开
+        _address = f'{file_folder_name}\\{file_name}'
+        if os.path.exists(_address) and os.path.isdir(_address):
+            _file = os.listdir(_address)
+            _len = len(_file)
+            print(file_name+'：'+str(_len)+'篇')
+            file_obj.write(file_name+'：'+str(_len)+'篇'+'\n')
         # for item_name in _file:
         #     file_obj.write(item_name+'\n')
